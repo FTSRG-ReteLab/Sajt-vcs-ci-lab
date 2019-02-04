c08119fac264d3f34794c2dd915e8b5ebf9e3569
matthewtuby <csokemate96@gmail.com>
2019-02-04 16:15:27 +0100

    Added some syntax errors.

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -4,6 +4,7 @@ import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
 
+    public;
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 5;
