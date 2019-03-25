f8668b16ed34cc4a0717ede8093a8083a1a96aaf
MatthewTuby <tubyluly@protonmail.com>
2019-03-25 07:06:10 -0700

    removed type

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -26,7 +26,7 @@ public class TrainUserImpl implements TrainUser {
 	public void overrideJoystickPosition(int joystickPosition) {
 		this.joystickPosition = joystickPosition;
 		controller.setJoystickPosition(joystickPosition);
-		new thread(controller).run();
+		new Thread((Runnable) controller).run();
 	}
 
 }
